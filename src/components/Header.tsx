32bf5cbc5bdaa0621aa1b2674ea9b0deb1d3d22c
fire-cat-cookie <168744994+fire-cat-cookie@users.noreply.github.com>
2024-07-25 19:46:36 +0200

    add top navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,17 @@
+import "./Header.scss";
+
 type Props = {
   title: string;
 };
 
 export default function Header({ title }: Props) {
-  return <h1>{title}</h1>;
+  return (
+    <div className="top-navbar">
+      <h1>{title}</h1>
+      <div className="top-navbar-nav">
+        <a>Character Builder</a>
+        <a>Game View</a>
+      </div>
+    </div>
+  );
 }
